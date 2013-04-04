b179a38d5c9e6d97866cd4f920f717189682d239
Duane R. Bailey <bailey.d.r@gmail.com>
2013-04-04 10:02:38 -0400

    Fixed courses and seeding.

--- lib/section_loader.rb
+++ lib/section_loader.rb
@@ -3,24 +3,23 @@ require 'csv'
 def load_sections file, logger
   rows = CSV.read(file)
   rows.shift # pop off the header
-  #SpireID, Dept, Number, StartTime, EndTime, Associated, SectionNum, PrimaryClass, Instructor, CourseName
+  #SpireID, Dept, Number, Days, StartTime, EndTime, Associated, SectionNum, PrimaryClass, Instructor, CourseName, GenEd
   rows.each_index do |idx|
     line = idx + 2 # we popped off header.
     row = rows[idx]
     begin
-      if row.length != 10
+      if row.length != 12
         raise "Bad number of entries in CSV row #{line}."
-        next
       end
       class_num_info = row[2].split
       dept = class_num_info[0]
       num_string = class_num_info[1]
       num = num_string[0..2]
       class_string = dept + num_string
-      name = row[9]
+      name = row[10]
 
       # :dept, :desc, :name, :number, :sections, :string
-      course = Course.where('number = (?) AND dept = (?)', num, dept).first
+      course = Course.where('number = ? AND dept = ?', num, dept).first
       if course.nil?
         course = Course.new
         course.dept = dept
@@ -33,10 +32,10 @@ def load_sections file, logger
 
       section = Section.new
       section.course = course
-      section.instructor = row[8]
+      section.instructor = row[9]
       section.section_number = row[6]
       section.spire_id = row[0]
-      if row[7] # primary
+      if row[8] # primary
         section.ty = 'LEC' # could also be sem
       else
         section.ty = 'unknown'

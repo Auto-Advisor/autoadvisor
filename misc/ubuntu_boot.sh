a8c92b9af66077abd992331653b4bc06115c63e9
Duane R. Bailey <bailey.d.r@gmail.com>
2013-04-03 20:56:32 -0400

    Removed sqlite3 from dependencies.

--- misc/ubuntu_boot.sh
+++ misc/ubuntu_boot.sh
@@ -1,4 +1,4 @@
-sudo apt-get install git build-essential openssh-client ruby1.9.1-dev libsqlite3-dev sqlite3 libmysqlclient-dev mysql-client mysql-server libv8-dev nodejs && \
+sudo apt-get install git build-essential openssh-client ruby1.9.1-dev libmysqlclient-dev mysql-client mysql-server libv8-dev nodejs && \
   git clone git@github.com:Auto-Advisor/autoadvisor.git autoadvisor && \
   cd autoadvisor && \
   sudo gem install rails && \

468727994c334a0a275f1f62a5820cd9f35baa82
Ken Chan <chankiyu22@chankiyu22-virtual-machine.(none)>
2013-04-10 14:43:22 -0400

    Improved UI

--- app/assets/javascripts/schedule.js
+++ app/assets/javascripts/schedule.js
@@ -19,3 +19,34 @@ $(document).ready(function(){
   });
 });
 
+function printDayOff(days) {
+  var day = '';
+  for (var i = 0; i < days.length; i++) {
+    if (days[i].checked) {
+      day += days[i].value;
+    }
+  }
+  return day;
+}
+
+$('#search').bind('click', function (event) {
+  var result = '<b>Your Search Criterial</b><br />Course Name/Number: '
+             + $('input#cnum').val() + '<br />'
+             + 'Course From: ' 
+             + $('select#major').val() + $('input#csmall').val()
+             + ' to '
+             + $('select#major').val() + $('input#clarge').val() + '<br />'
+             + 'Time from: '
+             + $('select#tfrom').val()
+             + ' to '
+             + $('select#tto').val() + '<br />'
+             + 'Day-off: '
+             + printDayOff($('.day'))
+          // + a list of tables
+               ;
+  $('div#list_class').html(result);
+});
+
+$('#recommend').bind('click', function (event) {
+  $('div#recommendations').html('This is your recommendation');
+});

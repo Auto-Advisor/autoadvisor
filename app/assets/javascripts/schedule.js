7d53fd200c152c5b0479b1d09449d4758f609ffd
Ken Chan <chankiyu22@chankiyu22-virtual-machine.(none)>
2013-04-13 05:33:32 -0400

    a small push :D

--- app/assets/javascripts/schedule.js
+++ app/assets/javascripts/schedule.js
@@ -25,12 +25,37 @@ $(document).ready(function(){
 
 $('#major').prop('selectedIndex', -1);
 
-$('#search').bind('click', function (event) {
+$('#class_1').bind('click', function (event) {
+  var details = $('.details');
+  for (var i = 0; i < details.length; i++) {
+    details[i].style.display = 'block';
+  }
+});
 
+$('#search').bind('click', function (event) {
+  var string = '{';
+  if($('#cnum').val()) {
+    string += '"course_number_or_name": "' + $('#cnum').val() + '", ';
+  }
+  if($('#major').val()) {
+    string += '"major": "' + $('#major').val() + '", ';
+  }
+  if($('#csmall').val()) {
+    string += '"course_from": "' + $('#csmall').val() + '", ';
+    if ($('#clarge').val()) {
+      string += '"course_to": "' + $('#clarge').val() + '", ';
+    }
+    else{
+      string += '"course_to": "' + $('#csmall').val() + '", ';
+    }
+  }
+  string += '"time_from": "' + $('#time_from').val() + '", ';
+  string += '"time_to": "' + $('#time_to').val() + '", ';
+  string += '"day": "' + getDays('day') + '"';
+  string += '}';
+  $('#deg').html(string);
 });
 
 $('#clear').bind('click', function (event) {
-  for (var i = 1; i < 8; i++){
-    $('#day_' + i).checked = false;
-  }
+  $('#deg').html('hi');
 });

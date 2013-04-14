8c5cd7500399411002c3932f4b3835e92e304bf2
Wei <wwlin@student.umass.edu>
2013-04-13 20:35:29 -0400

    advance search tab bug fix

--- app/assets/javascripts/schedule.js
+++ app/assets/javascripts/schedule.js
@@ -1,23 +1,24 @@
 function show_filter() {
   document.getElementById('filter_class').style.display = 'block';
   document.getElementById('recommendation').style.display = 'none';
-  document.getElementByID('adv_search').style.display = 'none';
+  document.getElementById('adv_search').style.display = 'none';
 }
 
 function show_recommendation() {
   document.getElementById('filter_class').style.display = 'none';
   document.getElementById('recommendation').style.display = 'block';
-  document.getElementByID('adv_search').style.display = 'none';
+  document.getElementById('adv_search').style.display = 'none';
 }
 
 function adv_filter(){
   document.getElementById('filter_class').style.display = 'none';
   document.getElementById('recommendation').style.display = 'none';
-  document.getElementByID('adv_search').style.display = 'block';
+  document.getElementById('adv_search').style.display = 'block';
 }
 function showClass() {
   document.getElementById('list_class').style.display = 'block';
 }
+
 
 $(document).ready(function(){
   $('button#course_description').click(function(){

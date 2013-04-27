f93fa45d3abb8afeb941d840086ff9a0141047d2
GAO <gao@ubuntu.ubuntu-domain>
2013-04-27 17:53:04 -0400

    minor fixes

--- app/assets/javascripts/schedule_chart.js
+++ app/assets/javascripts/schedule_chart.js
@@ -1,18 +1,12 @@
-var classes;
-
-function getClasses(c){
- classes=c;
-}
 
 function searchSlotMon(t){
-	var classes_json = {"classes":[{"name":"CMPSCI 311","time":"8:00AM-9:15AM" },{"name":"CMPSCI 383","time":"11:15AM-12:30PM" },{"name":"CMPSCI 320","time":"4:00PM-5:15PM"},{"name":"CMPSCI 377","time":"2:30PM-3:45PM" }]};
+	var classes_json = {"classes":[{"name":"CMPSCI 240","time":"9:05AM-9:55AM" },{"name":"CMPSCI 383","time":"1:25-2:15PM" },{"name":"CMPSCI 320","time":"2:30PM-3:20PM"}]};
 	
 	for(var i = 0; i < classes_json.classes.length; i++){
 	console.log(classes_json.classes[i].time.charAt(0) + ' ' + t.charAt(0));
 		if(classes_json.classes[i].time.charAt(0)  === t.charAt(0) && 
 		classes_json.classes[i].time.charAt(1)  === t.charAt(1))
-			return classes_json.classes[i].name;
-			
+			return classes_json.classes[i].name + '<br />' + classes_json.classes[i].time;		
 	}
 	return;
 			
@@ -21,28 +15,26 @@ function searchSlotMon(t){
 
 
 function searchSlotTues(t){
-	var classes_json = {"classes":[{"name":"CMPSCI 311","time":"8:00AM-9:15AM" },{"name":"CMPSCI 383","time":"11:15AM-12:30PM" },{"name":"CMPSCI 320","time":"4:00PM-5:15PM"},{"name":"CMPSCI 377","time":"2:30PM-3:45PM" }]};
+	var classes_json = {"classes":[{"name":"CMPSCI 326","time":"4:00PM-5:15PM" },{"name":"CMPSCI 513","time":"1:00PM-2:15PM" }]};
 	
 	for(var i = 0; i < classes_json.classes.length; i++){
 	console.log(classes_json.classes[i].time.charAt(0) + ' ' + t.charAt(0));
 		if(classes_json.classes[i].time.charAt(0)  === t.charAt(0) && 
 		classes_json.classes[i].time.charAt(1)  === t.charAt(1))
-			return classes_json.classes[i].name;
-			
+			return classes_json.classes[i].name + '<br />' + classes_json.classes[i].time;		
 	}
 	return;
 			
 }
 
 function searchSlotWed(t){
-	var classes_json = {"classes":[{"name":"CMPSCI 311","time":"8:00AM-9:15AM" },{"name":"CMPSCI 383","time":"11:15AM-12:30PM" },{"name":"CMPSCI 320","time":"4:00PM-5:15PM"},{"name":"CMPSCI 377","time":"2:30PM-3:45PM" }]};
+	var classes_json = {"classes":[{"name":"CMPSCI 240","time":"9:05AM-9:55AM" },{"name":"CMPSCI 383","time":"1:25-2:15PM" },{"name":"CMPSCI 320","time":"2:30PM-3:20PM"}]};
 	
 	for(var i = 0; i < classes_json.classes.length; i++){
 	console.log(classes_json.classes[i].time.charAt(0) + ' ' + t.charAt(0));
 		if(classes_json.classes[i].time.charAt(0)  === t.charAt(0) && 
 		classes_json.classes[i].time.charAt(1)  === t.charAt(1))
-			return classes_json.classes[i].name;
-			
+			return classes_json.classes[i].name + '<br />' + classes_json.classes[i].time;	
 	}
 	return;
 			
@@ -50,28 +42,26 @@ function searchSlotWed(t){
 
 
 function searchSlotThur(t){
-	var classes_json = {"classes":[{"name":"CMPSCI 311","time":"8:00AM-9:15AM" },{"name":"CMPSCI 383","time":"11:15AM-12:30PM" },{"name":"CMPSCI 320","time":"4:00PM-5:15PM"},{"name":"CMPSCI 377","time":"2:30PM-3:45PM" }]};
+	var classes_json = {"classes":[{"name":"CMPSCI 326","time":"4:00PM-5:15PM" },{"name":"CMPSCI 513","time":"1:00PM-2:15PM" }]};
 	
 	for(var i = 0; i < classes_json.classes.length; i++){
 	console.log(classes_json.classes[i].time.charAt(0) + ' ' + t.charAt(0));
 		if(classes_json.classes[i].time.charAt(0)  === t.charAt(0) && 
 		classes_json.classes[i].time.charAt(1)  === t.charAt(1))
-			return classes_json.classes[i].name;
-			
+			return classes_json.classes[i].name + '<br />' + classes_json.classes[i].time;		
 	}
 	return;
 			
 }			
 			
 function searchSlotFri(t){
-	var classes_json = {"classes":[{"name":"CMPSCI 311","time":"8:00AM-9:15AM" },{"name":"CMPSCI 383","time":"11:15AM-12:30PM" },{"name":"CMPSCI 320","time":"4:00PM-5:15PM"},{"name":"CMPSCI 377","time":"2:30PM-3:45PM" }]};
+	var classes_json = {"classes":[{"name":"CMPSCI 240","time":"9:05AM-9:55AM" },{"name":"CMPSCI 383","time":"1:25-2:15PM" },{"name":"CMPSCI 320","time":"2:30PM-3:20PM"}]};
 	
 	for(var i = 0; i < classes_json.classes.length; i++){
 	console.log(classes_json.classes[i].time.charAt(0) + ' ' + t.charAt(0));
 		if(classes_json.classes[i].time.charAt(0)  === t.charAt(0) && 
 		classes_json.classes[i].time.charAt(1)  === t.charAt(1))
-			return classes_json.classes[i].name;
-			
+			return classes_json.classes[i].name + '<br />' + classes_json.classes[i].time;		
 	}
 	return;
 			

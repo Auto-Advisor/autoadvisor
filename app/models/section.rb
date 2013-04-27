542d3a501017f57497bb6e58af6f45a0201a55ea
Duane R. Bailey <bailey.d.r@gmail.com>
2013-04-27 12:26:10 -0400

    Basic queries hopefully working...?

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,9 +1,22 @@
+HOURS = 24
+MINUTES = 60
+
 class Section < ActiveRecord::Base
   attr_accessible :days, :gened, :instructor, :min_end, :min_start, :room, :section_number, :size, :spire_id, :ty, :units
 
   belongs_to :time_slot
   belongs_to :course
   has_and_belongs_to_many :users
+
+   def self.time_str time
+    hours = time / MINUTES
+    minutes = time % MINUTES
+    meridian = ""
+    meridian = hours < 12 ? "AM" : "PM"
+    hours = 12 if hours == 0
+    hours -= 12 if hours > 12
+    return "%02d:%02d%s" % [hours, minutes, meridian]
+  end
 
   def dept
     course.dept
@@ -23,6 +36,26 @@ class Section < ActiveRecord::Base
 
   def desc
     course.desc
+  end
+
+  def start_s
+    Section.time_str min_start
+  end
+
+  def end_s
+    Section.time_str min_end
+  end
+
+  def range_s
+    "#{start_s} - #{end_s}"
+  end
+
+  def time_place_s
+    "#{start_s} - #{end_s} #{days}"
+  end
+
+  def self.max_time
+    HOURS * MINUTES
   end
 
   # fields:
@@ -66,36 +99,58 @@ class Section < ActiveRecord::Base
   def self.sections_for_constraints(constraints)
     query = Section.joins(:course)
     constraints.each do |constraint|
+      invert = constraint.include? "invert" && constraint["invert"] == true
+      eq_op = invert ? "!=" : "="
+      lt_op = invert ? ">" : "<="
+      gt_op = invert ? "<" : ">="
+      not_op = invert ? "" : "NOT"
+      yes_op = invert ? "NOT" : ""
+      and_op = invert ? " OR " : " AND "
+      or_op = invert ? " AND " : " OR "
       case constraint["type"]
       when "major"
-        query = query.where("courses.dept = ?", constraint["major"])
+      query = query.where("courses.dept #{eq_op} ?", constraint["major"])
       when "c_time"
-        # needs: section.beg, section.end
+        lower = constraint["lower"]
+        upper = constraint["upper"]
+        query = query.where("sections.min_beg #{gt_op} ? #{and_op} sections.min_end #{lt_op} upper", lower, upper)
       when "credit"
-        # need: sec.min_credit, sec.max_credit, or digits of possible credits.
+        lower = constraint["lower"]
+        upper = [constraint["upper"], 6].min
+        possible = [lower..upper].to_a
+        credits = possible.map {|potential| "units #{yes_op} LIKE CONCAT('_',?,'_')"}
+        complete = "(%s)" % credits.join(" #{or_op} ")
+        query = query.where(complete, possible.map {|i|i.to_s})
       when "num_courses"
+        lower = constraint["lower"] || 0
+        upper = constraint["upper"] || Section.max_time
       when "spe_course"
         # needs: to be explained?
-      when "dayoff"
-        # needs: section.days
+      when "daysoff"
+        daysoff = constraint["daysoff"]
+
+        daysoff.each_char do |c|
+          query = query.where("sections.days #{not_op} LIKE '%#{c}%'")
+        end
       when "course_range"
         case constraint["operation"]
         when "<="
-          query = query.where("courses.number <= ?", constraint["input"].to_i)
+          query = query.where("courses.number #{lt_op} ?", constraint["input"].to_i)
         when ">="
-          query = query.where("courses.number >= ?", constraint["input"].to_i)
+          query = query.where("courses.number #{gt_op} ?", constraint["input"].to_i)
         when "="
-          query = query.where("courses.number = ?", constraint["input"].to_i)
+          query = query.where("courses.number #{eq_op} ?", constraint["input"].to_i)
         end
       when "dis_lab"
         if constraint["discussion"]
-          query = query.where("sections.ty = 'DIS'")
+          query = query.where("sections.ty #{eq_op} 'DIS'")
         end
         if constraint["lab"]
-          query = query.where("sections.ty = 'LAB'")
+          query = query.where("sections.ty #{eq_op} 'LAB'")
         end
       when "gen"
-        # needs: section.gen
+        # gened is assumed to be a bang-separated list of codes.
+        query = query.where("sections.gened #{yes_op} LIKE CONCAT('!',?,'!')", constraints["gen"])
       end
     end
     query
